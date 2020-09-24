9c9d8b6ae120617fd14e749d403f0385cd288893
Todd Wallace <todd@Todds-MacBook-Pro.local>
2020-09-24 07:14:47 -0400

    fixed it again

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ form.addEventListener('submit', function(event) {
         var result = x / y;
         var percent = result * 100;
         
-        resultField.innerText = "Answer: " + percent + "%";
+        resultField.innerText = "answer: " + percent + "%";
         event.preventDefault();
     }
 });
